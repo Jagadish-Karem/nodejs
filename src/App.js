a4d622da4128d3df94af122fcf673728e875c311
pratyusha92 <pratyusha.p@mavensoft.com>
2018-03-23 16:38:18 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+class App extends Component {
+  render() {
+    return (
+      <body>
+      <div class="box">
+      
+        <p >
+         Using Sass In ReactJs
+        </p>
+
+        <p >
+React with Sass
+          </p>
+      </div>
+      </body>
+    );
+  }
+}
+
+export default App;
